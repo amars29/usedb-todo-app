fb3897377e832fc9627fc75d20fa63528334c096
Amar Somani <amars@geekyants.com>
2021-02-23 16:44:21 +0530

    initial commit

---
+++ config/usedb.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'middleware' => [],
+    "modelPath" => "App\Models\\",
+    'permissions' => [
+        'gates' => [
+
+            // 'modelName' => [
+            //     'update' => [],
+            //     'delete' => [],
+            //     'create' => [],
+            //     'findOne' => [],
+            //     'findMany' => []
+            // ]
+
+        ],
+        'policies' => [
+
+            // 'modelName' => [
+            //     'update' => '',
+            //     'delete' => '',
+            //     'create' => '',
+            //     'findOne' => '',
+            //     'findMany' => ''
+            // ]
+        ]
+    ]
+];
